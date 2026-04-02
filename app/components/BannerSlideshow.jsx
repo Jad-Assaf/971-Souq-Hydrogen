4498f98b356fc1a0befc643214abeb69ae64d9f7
Jad A. <73811148+Jad-Assaf@users.noreply.github.com>
2026-04-02 15:35:31 +0300

    banners

--- app/components/BannerSlideshow.jsx
+++ app/components/BannerSlideshow.jsx
@@ -221,7 +221,7 @@ const styles = {
     objectFit: 'contain',
     maxWidth: '1500px',
     margin: 'auto',
-    borderRadius: '20px',
+    borderRadius: '10px',
   },
   link: {
     width: '100vw',
